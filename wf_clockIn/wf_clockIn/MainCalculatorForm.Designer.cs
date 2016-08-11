60d3bc0dd244fe6d7ad8503007283877776850f1
Kang Son <shawn9405@gmail.com>
2016-08-10 20:41:59 -0700

    All buttons works properly, Enter button needs Code.

--- wf_clockIn/wf_clockIn/MainCalculatorForm.Designer.cs
+++ wf_clockIn/wf_clockIn/MainCalculatorForm.Designer.cs
@@ -51,7 +51,7 @@
             this.btn0 = new System.Windows.Forms.Button();
             this.btnAllClear = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.idDisplay = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -81,14 +81,14 @@
             // saveExitToolStripMenuItem
             // 
             this.saveExitToolStripMenuItem.Name = "saveExitToolStripMenuItem";
-            this.saveExitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveExitToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.saveExitToolStripMenuItem.Text = "Query";
             this.saveExitToolStripMenuItem.Click += new System.EventHandler(this.saveExitToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -153,12 +153,13 @@
             this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn1.Location = new System.Drawing.Point(42, 332);
-            this.btn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(90, 90);
             this.btn1.TabIndex = 1;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn2
             // 
@@ -167,12 +168,13 @@
             this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn2.Location = new System.Drawing.Point(136, 333);
-            this.btn2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn2.Margin = new System.Windows.Forms.Padding(2);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(90, 90);
             this.btn2.TabIndex = 2;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn3
             // 
@@ -181,12 +183,13 @@
             this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn3.Location = new System.Drawing.Point(230, 332);
-            this.btn3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn3.Margin = new System.Windows.Forms.Padding(2);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(90, 90);
             this.btn3.TabIndex = 3;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btn4
             // 
@@ -195,12 +198,13 @@
             this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn4.Location = new System.Drawing.Point(42, 239);
-            this.btn4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn4.Margin = new System.Windows.Forms.Padding(2);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(90, 90);
             this.btn4.TabIndex = 4;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn5
             // 
@@ -209,12 +213,13 @@
             this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn5.Location = new System.Drawing.Point(136, 239);
-            this.btn5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn5.Margin = new System.Windows.Forms.Padding(2);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(90, 90);
             this.btn5.TabIndex = 5;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn6
             // 
@@ -223,12 +228,13 @@
             this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn6.Location = new System.Drawing.Point(230, 239);
-            this.btn6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn6.Margin = new System.Windows.Forms.Padding(2);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(90, 90);
             this.btn6.TabIndex = 6;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btn7
             // 
@@ -237,12 +243,13 @@
             this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn7.Location = new System.Drawing.Point(42, 145);
-            this.btn7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn7.Margin = new System.Windows.Forms.Padding(2);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(90, 90);
             this.btn7.TabIndex = 7;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn8
             // 
@@ -251,12 +258,13 @@
             this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn8.Location = new System.Drawing.Point(136, 145);
-            this.btn8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn8.Margin = new System.Windows.Forms.Padding(2);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(90, 90);
             this.btn8.TabIndex = 8;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn9
             // 
@@ -265,12 +273,13 @@
             this.btn9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn9.Location = new System.Drawing.Point(230, 145);
-            this.btn9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn9.Margin = new System.Windows.Forms.Padding(2);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(90, 90);
             this.btn9.TabIndex = 9;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // btn0
             // 
@@ -279,12 +288,13 @@
             this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn0.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btn0.Location = new System.Drawing.Point(136, 428);
-            this.btn0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn0.Margin = new System.Windows.Forms.Padding(2);
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(90, 90);
             this.btn0.TabIndex = 10;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btnAllClear
             // 
@@ -293,12 +303,13 @@
             this.btnAllClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 27F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAllClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btnAllClear.Location = new System.Drawing.Point(42, 428);
-            this.btnAllClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAllClear.Margin = new System.Windows.Forms.Padding(2);
             this.btnAllClear.Name = "btnAllClear";
             this.btnAllClear.Size = new System.Drawing.Size(90, 90);
             this.btnAllClear.TabIndex = 11;
             this.btnAllClear.Text = "AC";
             this.btnAllClear.UseVisualStyleBackColor = false;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
             // 
             // btnClear
             // 
@@ -307,24 +318,29 @@
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
             this.btnClear.Location = new System.Drawing.Point(230, 428);
-            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(90, 90);
             this.btnClear.TabIndex = 12;
             this.btnClear.Text = "Enter";
             this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
-            // textBox1
+            // idDisplay
             // 
-            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(203)))), ((int)(((byte)(67)))));
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 41F);
-            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(101)))), ((int)(((byte)(101)))));
-            this.textBox1.Location = new System.Drawing.Point(11, 58);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(340, 69);
-            this.textBox1.TabIndex = 13;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.idDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(203)))), ((int)(((byte)(67)))));
+            this.idDisplay.Cursor = System.Windows.Forms.Cursors.Default;
+            this.idDisplay.Enabled = false;
+            this.idDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 41F, System.Drawing.FontStyle.Underline);
+            this.idDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(95)))), ((int)(((byte)(95)))), ((int)(((byte)(95)))));
+            this.idDisplay.Location = new System.Drawing.Point(11, 58);
+            this.idDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.idDisplay.MaxLength = 100;
+            this.idDisplay.Name = "idDisplay";
+            this.idDisplay.ReadOnly = true;
+            this.idDisplay.Size = new System.Drawing.Size(340, 69);
+            this.idDisplay.TabIndex = 13;
+            this.idDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // MainCalculatorForm
             // 
@@ -333,7 +349,7 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(134)))), ((int)(((byte)(134)))));
             this.BackgroundImage = global::wf_clockIn.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(362, 527);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.idDisplay);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnAllClear);
             this.Controls.Add(this.btn0);
@@ -385,7 +401,7 @@
         private System.Windows.Forms.Button btn0;
         private System.Windows.Forms.Button btnAllClear;
         private System.Windows.Forms.Button btnClear;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox idDisplay;
     }
 }
 
--- wf_clockIn/wf_clockIn/MainCalculatorForm.cs
+++ wf_clockIn/wf_clockIn/MainCalculatorForm.cs
@@ -5,20 +5,16 @@ namespace wf_clockIn
 {
     public partial class MainCalculatorForm : Form
     {
-        //private int studentID;
-        //private Calendar timeIn;
-        //private Calendar timeOut;
-
-        // search for calendar like api for C sharp
-
         public MainCalculatorForm()
         {
             InitializeComponent();
+            idDisplay.Text = "";
+            idDisplay.MaxLength = 10; // Recommanded. If more needed, Please Widen the idDisplay box.
         }
-
+        
 
         #region Group of Toolstrip menu Items.
-         
+
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -49,6 +45,91 @@ namespace wf_clockIn
         private void logToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Need to show different form that will be able to Modify the Log history Database.
+        }
+
+        #endregion
+        
+
+        // Helper method for when [Enter] is clicked || User hits the Enter Key.
+        private void studentClockIn()
+        {
+            // Validate the student ID.
+            // If it's Valid ID  -> Clock In with Message.
+            // If it's NOT Valid -> Change display Box color to Red / Show Message to Re-Enter.
+        }
+
+        // Helper method to write number to idDisplay box.
+        private void writeNumber(int number)
+        {
+            if (idDisplay.Text.Length < 10)
+            {
+                idDisplay.Text += (""+ number);
+            }
+        }
+
+        #region Button Click Method group.
+
+        // If [AC] button is clicked.
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            // Remove all the text from the ID display.
+            idDisplay.Text = "";
+        }
+        // If [Enter] button is clicked.
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Call studentClockIn() function. (helper method)
+            studentClockIn();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            writeNumber(0);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            writeNumber(1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            writeNumber(2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            writeNumber(3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            writeNumber(4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            writeNumber(5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            writeNumber(6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            writeNumber(7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            writeNumber(8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            writeNumber(9);
         }
 
         #endregion
